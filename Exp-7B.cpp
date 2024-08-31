b35f055761eaea03317c0261415ef2be0eb9f20b
SohamMangale <sohammangale60@gmail.com>
2024-08-31 19:10:01 +0530

    Create Exp-7B.cpp

---
+++ Exp-7B.cpp
@@ -0,0 +1,18 @@
+//Soham
+//entc B1
+//23070123084
+//experiment 7B
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,i,j=0,k,l,temp;
+    cout<<"Enter size of array";
+    cin>>n;
+    int a1[n],a2[n];
+    for (i=0;i<n;i++)
+    {
+        cout<<"Enter element-"<<i+1<<": ";
+        cin>>a1[i];
+    }
+} 
