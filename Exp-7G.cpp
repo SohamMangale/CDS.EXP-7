030fd05a9c6fd4b1479f10bd8f8f79f1de6f01a6
SohamMangale <sohammangale60@gmail.com>
2024-09-05 22:09:24 +0530

    Create Exp-7G.cpp

---
+++ Exp-7G.cpp
@@ -0,0 +1,14 @@
+//Soham
+//entc B1
+//23070123084
+//experiment 7G
+#include <iostream>
+#include<string>
+using namespace std;
+int main(){
+    string a;
+    cout<<"enter any word: ";
+    cin>>a;
+    cout<<" entered string is "<<a<<endl;
+    return 0;
+}
